b48ee8b50a1129fafcc68707c49d10ad3bfe9350
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-03-02 11:37:50 -0300

    Adding the property for remove the mode banner debug

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -7,6 +7,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MensagemView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
