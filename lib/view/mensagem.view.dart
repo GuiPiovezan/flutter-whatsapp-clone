a4d190d278471145518452586b369a2bed9729c3
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-03-02 12:15:19 -0300

    Added the widget appBar, the body with Component created (CardMessage) and the bottomNavigatorBar

--- lib/view/mensagem.view.dart
+++ lib/view/mensagem.view.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/view/components/card.message.dart';
 
 class MensagemView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          CircleAvatar(
-            backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Message"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: ListView(
+        children: <Widget>[
+          CardMessage(),
+          CardMessage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        fixedColor: Colors.green,
+        unselectedIconTheme: IconThemeData(
+          color: Colors.grey[500],
+        ),
+        showSelectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.circle_notifications),
+            label: "Status",
           ),
-          Column(
-            children: [
-              Text("Nome"),
-              Text("Mensagem"),
-            ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone_outlined),
+            label: "Phone",
           ),
-          Column(
-            children: [
-              Text("18:00"),
-              Text("2"),
-            ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: "Messages",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
           ),
         ],
       ),
