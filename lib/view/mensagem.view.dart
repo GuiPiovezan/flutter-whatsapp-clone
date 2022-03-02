465c64aeb8ddeabce1ad8a0c6fd253575828703f
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-03-02 18:50:04 -0300

    Added the property const

--- lib/view/mensagem.view.dart
+++ lib/view/mensagem.view.dart
@@ -6,7 +6,7 @@ class MensagemView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Message"),
+        title: const Text("Message"),
         centerTitle: true,
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
